1bf488ef5f427db950f1e314a24c31563ffd552b
Bryce <brycemwilson@gmail.com>
2015-12-12 02:40:26 -0600

    Add segment component

---
+++ Assets/Scripts/Coccus.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coccus : Segment {
+	public AudioClip touchSound;
+	public AnimationCurve growCurve;
+
+	protected bool growing;
+	protected float growTime;
+
+	// Use this for initialization
+	protected override void Start () {
+		base.Start ();
+		growing = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (growing) {
+			growTime += Time.deltaTime;
+			if(growTime >= growCurve.keys[growCurve.length-1].time) 	{
+				growTime = growCurve.keys[growCurve.length-1].time;
+			}
+			float s = growCurve.Evaluate(growTime);
+			transform.localScale = new Vector3(s, s, s);
+		}
+	}
+
+	public override void Grow ()
+	{
+		growing = true;
+	}
+
+	public override void OnTouch ()
+	{
+		soundSrc.clip = touchSound;		
+		soundSrc.Play ();
+	}
+}
---
+++ Assets/Scripts/Segment.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Segment : MonoBehaviour {
+	protected AudioSource soundSrc;
+
+	// Use this for initialization
+ 	protected virtual void Start () {
+		soundSrc = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// OnTouch is called when this segment is touched
+	public virtual void OnTouch () {
+
+	}
+
+	// Grow is called when this segment is to grow
+	public virtual void Grow () {
+
+	}
+}
